64986622affd2b7b1c5267c7b3b9c921a02b94c9
Bennet <bennet.strauch@miu.edu>
2025-02-07 10:21:17 -0600

    START DEPLOYMENT

--- backend/server.ts
+++ backend/server.ts
@@ -1,42 +1,40 @@
-import express, {json, urlencoded} from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import { userRouter } from './users/users.router';
-import { errorHandler, routerNotFoundHandler } from './utils/handlers';
-import { connectToDB } from './database/connection';
-import { goalRouter } from './goals/goals.router';
-import { checkToken } from './users/users.middleware';
-import { findSimilarGoals } from './database/queries';
-import { Goal, GoalModel } from './goals/goals.model';
-import { progressRouter } from './progress/progresses.router';
-
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { userRouter } from "./types/users/users.router";
+import { errorHandler, routerNotFoundHandler } from "./utils/handlers";
+import { connectToDB } from "./database/connection";
+import { goalRouter } from "./goals/goals.router";
+import { checkToken } from "./types/users/users.middleware";
+import { findSimilarGoals } from "./database/queries";
+import { Goal, GoalModel } from "./goals/goals.model";
+import { progressRouter } from "./progress/progresses.router";
 
 const app = express();
 
-connectToDB()
+connectToDB();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(json())
-app.use(urlencoded())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(json());
+app.use(urlencoded());
 
+app.use("/users", userRouter);
+app.use("/goals", checkToken, goalRouter);
+app.use("/progresses", checkToken, progressRouter);
 
-app.use('/users', userRouter)
-app.use('/goals', checkToken, goalRouter)
-app.use('/progresses', checkToken, progressRouter)
+app.use(routerNotFoundHandler);
+app.use(errorHandler);
 
-app.use(routerNotFoundHandler)
-app.use(errorHandler)
+app.listen(3000, () => console.log("Server listening on Port 3000"));
 
-
-app.listen(3000, () => console.log('Server listening on Port 3000'))
-
-
-async function test(){
-const vector = Array.from({ length: 1536 }, () => Math.random());
-const goal = await GoalModel.findOne({ _id: '676d5eaa1aa4b1c31543e47b',}) as Goal;
-console.log('goal', goal, 'embeddedName:', goal.embedded_name)
-findSimilarGoals(goal.embedded_name)
+async function test() {
+  const vector = Array.from({ length: 1536 }, () => Math.random());
+  const goal = (await GoalModel.findOne({
+    _id: "676d5eaa1aa4b1c31543e47b",
+  })) as Goal;
+  console.log("goal", goal, "embeddedName:", goal.embedded_name);
+  findSimilarGoals(goal.embedded_name);
 }
 
 // test()
---
+++ backend/types/users/users.controller.ts
@@ -0,0 +1,73 @@
+import { RequestHandler } from "express";
+import { ErrorWithStatus } from "../../utils/classes";
+import { User, UserModel } from "./users.model";
+import { StandardResponse } from "../standardResponse";
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { LoginResponse } from "../login/loginResponse";
+import { LoginRequest } from "../login/loginRequest";
+
+type RegisterReqHandler = RequestHandler<
+  unknown,
+  StandardResponse<string>,
+  User,
+  unknown
+>;
+
+export const register: RegisterReqHandler = async (req, res, next) => {
+  try {
+    const newUser = req.body;
+
+    const hashedPassword = await bcrypt.hash(newUser.password, 10);
+    const newUserHashed = { ...req.body, password: hashedPassword };
+
+    const savedUser = await UserModel.create(newUserHashed);
+    const newUserId = savedUser._id.toString();
+
+    res.status(201).json({ success: true, data: newUserId });
+
+    console.log("User created successfully: ", savedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+type LoginReqHandler = RequestHandler<
+  unknown,
+  LoginResponse,
+  LoginRequest,
+  unknown
+>;
+
+export const login: LoginReqHandler = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw new ErrorWithStatus("Invalid email", 401);
+    }
+
+    // Compare password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new ErrorWithStatus("Invalid password", 401);
+    }
+
+    // Generate token
+    const token = jwt.sign(
+      { _id: user._id, name: user.name, email: user.email },
+      process.env.SECRET_KEY_FOR_SIGNING_TOKEN,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    // Send token
+    res.status(200).json({ success: true, data: { token } });
+  } catch (err) {
+    next(err);
+  }
+};
