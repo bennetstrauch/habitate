cdf47997fbaafd61ae1cb602e89d26dd1260dd3f
Bennet <bennet.strauch@miu.edu>
2025-05-27 19:49:39 -0500

    third intention box implemented

--- frontend/src/app/reflections/display/r-intention-general-2.component.ts
+++ frontend/src/app/reflections/display/r-intention-general-2.component.ts
@@ -2,17 +2,28 @@ import { Component, inject } from '@angular/core';
 import { getRandomPhrase } from '../../utils/utils';
 import { DailyReflectionService } from '../daily-reflection.service';
 import { MatButton } from '@angular/material/button';
+import { ReflectionsService } from '../reflections.service';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-r-intention-general-2',
-  imports: [MatButton],
+  imports: [MatButton, MatFormField, MatLabel, MatInput],
   template: `
     <div class="card">
       <div [innerHTML]="intentionInstruction"></div>
-      <br />
+     
+      <br /> <br> <br/> 
+      <mat-form-field appearance="outline">
+        <mat-label>your sweet intention</mat-label>
+        <!-- could alternate lable as well # -->
+        <input matInput #userIntention type="text"/>
+      </mat-form-field>
+
       <button
         mat-button
-        (click)="handleNextStep()"
+        matStepperNext
+        (click)="setIntentionAndProceed(userIntention.value)"
       >
         Something good arrived
         <!-- #could alternate -->
@@ -23,6 +34,7 @@ import { MatButton } from '@angular/material/button';
   styles: ``,
 })
 export class RIntentionGeneral2Component {
+  readonly reflectionsService = inject(ReflectionsService);
   readonly dailyReflectionService = inject(DailyReflectionService);
 
   manifests = [
@@ -44,9 +56,9 @@ export class RIntentionGeneral2Component {
 
   intentionInstruction = `Now, <br>close your eyes ${
     userWasAlreadyAskedToCloseEyes ? 'again' : ''
-  }, 
-  and see 
-  ${getRandomPhrase(this.noResistance)} 
+  }, <br>
+  and see <br>
+  <i>${getRandomPhrase(this.noResistance)}</i>
   whether a <strong>simple intention</strong> for today or tomorrow 
   ${getRandomPhrase(this.manifests)}.`;
   // #could alternate simple : soft sweet little
@@ -56,10 +68,16 @@ export class RIntentionGeneral2Component {
     'No resistance, no effort, no strain, <br>just a simple intention that comes up by itself.';
 
     handleNextStep() {
-      this.dailyReflectionService.$currentStep.set('finalize')
-      userWasAlreadyAskedToCloseEyes = false;
+     
       console.log('userWasAlreadyAskedToCloseEyes set to false', userWasAlreadyAskedToCloseEyes);
     }
+
+      setIntentionAndProceed(userIntention: string) {
+    this.reflectionsService.setIntention(userIntention);
+
+     this.dailyReflectionService.$currentStep.set('finalize')
+      userWasAlreadyAskedToCloseEyes = false;
+  }
 }
 
 export let userWasAlreadyAskedToCloseEyes = false;
